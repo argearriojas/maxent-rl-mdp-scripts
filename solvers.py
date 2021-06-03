113683b6a822a1ac1a295ea6ebbd64d1c884f731
Argenis Arriojas <argenis@arriojas.net>
2021-06-02 23:11:52 -0400

    Add helper methods and solvers

---
+++ solvers.py
@@ -0,0 +1,199 @@
+import numpy as np
+
+from utils import get_transition_dynamics, get_mdp_generator
+
+
+def maze_solver(env, policy=None, steps=100, update_interval=20, gamma=1, tolerance=1e-2):
+    print(f'Solving maze')
+
+    dynamics, rewards = get_transition_dynamics(env)
+    rewards = dynamics.multiply(rewards).sum(axis=0)
+    policy = np.ones((env.nS, env.nA)) / env.nA if policy is None else policy
+    mdp_generator = get_mdp_generator(env, dynamics, policy)
+
+    Qi = np.zeros((1, env.nS * env.nA))
+
+    for i in range(1, steps+1):
+        Qj = mdp_generator.T.dot(Qi.T).T
+        Qi_k = rewards + gamma * Qj
+        err = np.abs(Qi_k - Qi).max()
+        Qi = Qi_k
+
+        if i % update_interval == 0:
+            policy = Qi.reshape((env.nS, env.nA))
+            policy = (policy == policy.max(axis=1)).astype(float)
+            policy = policy / policy.sum(axis=1)
+            mdp_generator = get_mdp_generator(env, dynamics, policy)
+
+        if err <= tolerance:
+            break
+
+    if i == steps:
+        print(f'Reached max steps. Err:{err}')
+    else:
+        print(f"Done in {i} steps")
+
+    Vi = np.multiply(Qi.reshape((env.nS, env.nA)), policy).sum(axis=1)
+
+    return dict(
+        Q=np.array(Qi).reshape((env.nS, env.nA)),
+        V=np.array(Vi).reshape((env.nS, 1)),
+        policy=np.array(policy)
+    )
+
+
+def softq_solver(env, prior_policy=None, steps=100, update_interval=20, beta=1, gamma=1, tolerance=1e-2):
+
+    dynamics, rewards = get_transition_dynamics(env)
+    rewards = dynamics.multiply(rewards).sum(axis=0)
+    prior_policy = np.ones((env.nS, env.nA)) / env.nA if prior_policy is None else prior_policy
+    mdp_generator = get_mdp_generator(env, dynamics, prior_policy)
+
+    Qi = np.zeros((1, env.nS * env.nA))
+
+    for i in range(1, steps+1):
+        Qj = np.log(mdp_generator.T.dot(np.exp(beta * Qi.T)).T) / beta
+        Qi_k = rewards + gamma * Qj
+        err = np.abs(Qi_k - Qi).max()
+        Qi = Qi_k
+
+        if err <= tolerance:
+            break
+
+    if i == steps:
+        print(f'Reached max steps. Err:{err}')
+    else:
+        print(f"Done in {i} steps")
+    
+    Vi = np.log(
+        np.multiply(prior_policy, np.exp(beta * Qi.reshape((env.nS, env.nA)))).sum(axis=1)
+    ) / beta
+
+    policy = np.multiply(prior_policy, np.exp(beta * (Qi.reshape((env.nS, env.nA)) - Vi)))
+
+    return dict(
+        Q=np.array(Qi).reshape((env.nS, env.nA)),
+        V=np.array(Vi).reshape((env.nS, 1)),
+        policy=np.array(policy)
+    )
+
+
+def z_solver(env, prior_policy=None, steps=100, rho=1, tolerance=1e-2):
+
+    if prior_policy is None:
+        prior_policy = np.ones((env.nS, env.nA)) / env.nA
+    dynamics, rewards = get_transition_dynamics(env)
+    rewards = dynamics.multiply(rewards).sum(axis=0)
+
+    Pbar = np.matrix((np.array(dynamics.toarray()).reshape((env.nS, env.nS, env.nA)) * prior_policy.reshape((1, env.nS, env.nA))).sum(axis=2))
+    G = np.matrix(np.diag(np.exp(rho * np.array(rewards.reshape((env.nS, env.nA)).mean(axis=1)).flatten())))
+    M = Pbar.dot(G)
+
+    z = np.matrix(np.ones((env.nS, 1)))
+
+    max_it = steps
+    for i in range(1, max_it + 1):
+        zk = z.T.dot(M).T
+        lz = np.linalg.norm(zk)
+        zk = zk / lz
+        err = np.abs((np.log(zk) - np.log(z))/rho).max()
+        z = zk
+        if err <= tolerance:
+            break
+
+    if i == max_it:
+        print(f'Reached max steps. Err:{err}')
+    else:
+        print(f"Done in {i} steps")
+
+    zV = np.matrix(np.log(z) / rho)
+
+    # we can generate a Boltzmann policy from the value function
+    policy = dynamics.multiply(np.exp(rho * zV)).sum(axis=0).reshape((env.nS, env.nA))
+    policy /= policy.sum(axis=1)
+
+    return dict(
+        V=np.array(zV).reshape((env.nS, 1)),
+        policy=np.array(policy),
+        eigenvalue=lz,
+        left_eigenvector=np.array(z)
+    )
+
+
+def u_solver(env, prior_policy=None, steps=100, beta=1, tolerance=1e-2):
+
+    if prior_policy is None:
+        prior_policy = np.ones((env.nS, env.nA)) / env.nA
+
+    # dynamics is a rectangular matrix. One row for each state (s,), one column for each (s, a)
+    dynamics, rewards = get_transition_dynamics(env)
+    rewards = dynamics.multiply(rewards).sum(axis=0)
+
+    # the mdp generator is a square matrix. One row for each (s, a), one column for each (s, a)
+    P = get_mdp_generator(env, dynamics, prior_policy)
+
+    # diagonal matrix
+    T = np.matrix(np.diag(np.exp(beta * np.array(rewards).flatten())))
+
+    # the twisted matrix
+    M = P.dot(T)
+
+    # finding left and right eigenvectors
+    u = np.matrix(np.ones((env.nS * env.nA, 1)))
+    v = np.matrix(np.ones((env.nS * env.nA, 1)))
+
+    max_it = steps
+    for i in range(1, max_it+1):
+        uk = u.T.dot(M).T
+        lu = np.linalg.norm(uk)
+        uk = uk / lu
+
+        # computing an error for convergence estimation
+        mask = np.logical_and(uk > 0, u > 0)
+        err = np.abs((np.log(uk[mask]) - np.log(u[mask]))/ beta).max() + np.logical_xor(uk > 0, u > 0).sum()
+
+        vk = M.dot(v)
+        lv = np.linalg.norm(vk)
+        vk = vk / lv
+
+        # computing an error for convergence estimation
+        mask = np.logical_and(vk > 0, v > 0)
+        err += np.abs((np.log(vk[mask]) - np.log(v[mask]))).max() + np.logical_xor(vk > 0, v > 0).sum()
+
+        # update the eigenvectors
+        u = uk
+        v = vk
+
+        if err <= tolerance:
+            break
+
+    if i == max_it:
+        print(f'Reached max steps. Err:{err}')
+    else:
+        print(f"Done in {i} steps")
+
+    theta = - np.log(lu)
+    v = v / v.sum()
+    u = u / u.T.dot(v)
+    
+    uQ = - steps * theta / beta + np.log(u) / beta
+    uV = - steps * theta / beta + np.matrix(
+        np.log(
+            np.multiply(
+                np.array(u).reshape((env.nS, env.nA)),
+                prior_policy
+            ).sum(axis=1)
+        )
+    ).T / beta
+
+    policy = np.array(u).reshape(prior_policy.shape) * prior_policy
+    policy = policy / policy.sum(axis=1).reshape((-1, 1))
+
+    return dict(
+        Q=np.array(uQ).reshape((env.nS, env.nA)),
+        V=np.array(uV).reshape((env.nS, 1)),
+        policy=np.array(policy),
+        eigenvalue=lu,
+        left_eigenvector=np.array(u),
+        right_eigenvector=np.array(v)
+    )
---
+++ utils.py
@@ -0,0 +1,112 @@
+import numpy as np
+from scipy.sparse import csr_matrix
+
+from gym.wrappers import TimeLimit
+from environment import MAPS, ModifiedFrozenLake
+
+
+def get_maze(maze='8x8zigzag', variant='walls'):
+    desc = np.array(MAPS[maze], dtype='c')
+
+    # make sure to normalize to walls
+    desc[desc == b'H'] = b'W'
+
+    # then apply the variant
+    if variant == 'nails':
+        desc[desc == b'W'] = b'N'
+    elif variant == 'holes':
+        desc[desc == b'W'] = b'H'
+
+    return desc
+
+
+def get_zigzag_maze(maze_size=1):
+    tile1 = np.array([
+        'FFFFFFW',
+        'FFFWFFW',
+        'WWWWFFW',
+        'WFFFFFW',
+        'FFWFFFF',
+        'FFWWWWW',
+        'FFFFFFW',
+        'FFFWFFW',
+        'WWWWFFW',
+        'WFFFFFW',
+        'FFWFFFF',
+        'FFWWWWW',
+        'FFFFFFF',
+        'FFFFFFF',
+    ], dtype='c')
+    tile2 = np.flip(tile1, axis=0)
+    nr, nc = tile1.shape
+    tile = np.empty((nr, nc*2), dtype='c')
+    tile[:, :nc] = tile1
+    tile[:, nc:] = tile2
+
+    nr, nc = tile.shape
+    desc = np.empty((nr, nc*maze_size), dtype='c')
+    
+    for i in range(maze_size):
+        desc[:, i*nc:(i+1)*nc] = tile
+
+    desc[0, 0] = b'S'
+    desc[1, -2] = b'G'
+    return desc[:, :-1]
+
+
+def get_environment(desc, max_steps=1000, stay_prob=0.):
+    env_src = ModifiedFrozenLake(
+        n_action=4, hot_edges=False, max_reward=0., min_reward=-1.5, step_penalization=1,
+        desc=desc, never_done=True, cyclic_mode=True, slippery=0, goal_attractor=stay_prob)
+    env = TimeLimit(env_src, max_episode_steps=max_steps)
+
+    return env
+
+
+def get_transition_dynamics(env, absorbing=False):
+
+    ncol = env.nS * env.nA
+    nrow = env.nS
+
+    shape = (nrow, ncol)
+
+    row_lst, col_lst, prb_lst, rew_lst = [], [], [], []
+
+    assert isinstance(env.P, dict)
+    for s_i, s_i_dict in env.P.items():
+        for a_i, outcomes in s_i_dict.items():
+            for prb, s_j, r_j, _ in outcomes:
+                col = s_i * env.nA + a_i
+
+                if absorbing and r_j == 0:
+                    s_j = s_i
+                row_lst.append(s_j)
+                col_lst.append(col)
+                prb_lst.append(prb)
+                rew_lst.append(r_j)
+                
+
+    dynamics = csr_matrix((prb_lst, (row_lst, col_lst)), shape=shape)
+    assert (dynamics.sum(axis=0) == 1).all()
+
+    rewards = csr_matrix((rew_lst, (row_lst, col_lst)), shape=shape)
+
+    return dynamics, rewards
+
+
+def get_mdp_generator(env, transition_dynamics, policy):
+    td_coo = transition_dynamics.tocoo()
+
+    rows, cols, data = [], [], []
+    for s_j, col, prob in zip(td_coo.row, td_coo.col, td_coo.data):
+        for a_j in range(env.nA):
+            row = s_j * env.nA + a_j
+            rows.append(row)
+            cols.append(col)
+            data.append(prob * policy[s_j, a_j])
+
+    nrow = ncol = env.nS * env.nA
+    shape = (nrow, ncol)
+    mdp_generator = csr_matrix((data, (rows, cols)), shape=shape)
+
+    return mdp_generator
